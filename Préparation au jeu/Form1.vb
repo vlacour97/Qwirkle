29e2b4fb6b35d24acf418233379695320971b117
Valentin Lacour <vlacour97@icloud.com>
2016-05-18 21:47:37 +0200

    Mise en place de la génération des combinaisons de depart

--- Préparation au jeu/Form1.vb
+++ Préparation au jeu/Form1.vb
@@ -38,7 +38,7 @@
         response = False
         Select Case nb_player
             Case 2
-                If (txt_box_fname1.Text.Length > 0 And txt_box_fname2.Text.Length > 0 And (txt_box_age1.Text) And IsNumeric(txt_box_age2.Text) And Not IsNumeric(txt_box_fname1.Text) And Not IsNumeric(txt_box_fname2.Text)) Then
+                If (txt_box_fname1.Text.Length > 0 And txt_box_fname2.Text.Length > 0 And IsNumeric(txt_box_age1.Text) And IsNumeric(txt_box_age2.Text) And Not IsNumeric(txt_box_fname1.Text) And Not IsNumeric(txt_box_fname2.Text)) Then
                     response = True
                 Else
                     response = False
@@ -110,13 +110,34 @@
         lbl_fourth_name.Text = users(3).getName
     End Sub
 
+    Sub showDeck(ByVal user As User)
+        user.generateDeck()
+        picbox_1.Image = My.Resources.ResourceManager.GetObject(user.getTokenName(user.getDeckArray(0)))
+        picbox_2.Image = My.Resources.ResourceManager.GetObject(user.getTokenName(user.getDeckArray(1)))
+        picbox_3.Image = My.Resources.ResourceManager.GetObject(user.getTokenName(user.getDeckArray(2)))
+        picbox_4.Image = My.Resources.ResourceManager.GetObject(user.getTokenName(user.getDeckArray(3)))
+        picbox_5.Image = My.Resources.ResourceManager.GetObject(user.getTokenName(user.getDeckArray(4)))
+        picbox_6.Image = My.Resources.ResourceManager.GetObject(user.getTokenName(user.getDeckArray(5)))
+        chbox1.Tag = user.getDeckArray(0)
+        chbox2.Tag = user.getDeckArray(1)
+        chbox3.Tag = user.getDeckArray(2)
+        chbox4.Tag = user.getDeckArray(3)
+        chbox5.Tag = user.getDeckArray(4)
+        chbox6.Tag = user.getDeckArray(5)
+    End Sub
+
     Private Sub btn_OK_Click(sender As Object, e As EventArgs) Handles btn_OK.Click
         If (formIsValid()) Then
             lbl_remplir.Visible = False
             showPlayerNames()
+            showDeck(users(0))
         Else
             lbl_remplir.Visible = True
         End If
+    End Sub
+
+    Private Sub btn_OK2_Click(sender As Object, e As EventArgs) Handles btn_OK2.Click
+
     End Sub
 End Class
 
@@ -126,6 +147,7 @@ Public Class User
     Dim name As String
     Dim age As Integer
     Dim nb_points As Integer
+    Dim deckArray(5) As Integer
 
     Sub New(ByVal nameValue As String, ByVal ageValue As Integer)
         name = nameValue
@@ -144,5 +166,72 @@ Public Class User
     Function getNbPoints() As Integer
         Return nb_points
     End Function
+
+    Sub generateDeck()
+        Dim tempColor As Integer
+        Dim tempForm As Integer
+        For counter As Integer = 0 To deckArray.Length - 1
+            tempColor = CInt(Int((6 * Rnd()) + 1)) * 10
+            tempForm = CInt(Int((6 * Rnd()) + 1))
+            deckArray(counter) = tempColor + tempForm
+        Next
+    End Sub
+
+    Function getDeckArray() As Integer()
+        Return deckArray
+    End Function
+
+    Function getTokenName(ByVal tokenCode As Integer)
+        Dim color As String
+        Dim form As String
+        Select Case tokenCode
+            Case 10 To 16
+                color = "Rouge"
+            Case 20 To 26
+                color = "Orange"
+            Case 30 To 36
+                color = "Jaune"
+            Case 40 To 46
+                color = "Vert"
+            Case 50 To 56
+                color = "Bleu"
+            Case 60 To 66
+                color = "Violet"
+            Case Else
+                Return "nothing"
+        End Select
+        Select Case tokenCode Mod 10
+            Case 1
+                form = "Rond"
+            Case 2
+                form = "Croix"
+            Case 3
+                form = "Losange"
+            Case 4
+                form = "Carre"
+            Case 5
+                form = "Etoile"
+            Case 6
+                form = "Trefle"
+            Case Else
+                Return "nothing"
+        End Select
+        Return form & color
+    End Function
+
+    'Indicateur de couleur
+    '10 : Rouge
+    '20 : Orange
+    '30 : Jaune
+    '40 : Vert
+    '50 : Bleu
+    '60 : Violet
+    'Indicateur de forme
+    '1 : Rond
+    '2 : Croix
+    '3 : Losange
+    '4 : Carré
+    '5 : Etoile
+    '6 : Plus
 
 End Class
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Qwirkle
+
+Projet Pedagogique pour l'IUT Informatique d'Amiens
+****Programmé en VB
